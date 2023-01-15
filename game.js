6a6fa303c7febd09a251bb5ac1eba24f04cac75a
Ayomidefln <ayomidefln@gmail.com>
2023-01-15 18:25:07 +0100

    More Functions

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-tex'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#scoreText');
+const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
-let availableQuestion = 0
+let availableQuestions = 0
 
 let questions = [
     {
@@ -53,29 +53,29 @@ let questions = [
      }
 ]
 
-const SCORE_POINT = 100
+const SCORE_POINTS = 100
 const MAX_QUESTIONS = 5
 
 startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestion = [...questions]
+    availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () =>{
-    if(availableQuestion.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostResentScore', score)
 
-        return window.location.assign('*/end.html')
+        return window.location.assign('/end.html')
     }
 
     questionCounter++
     progressText.innerText = `Question $(questionCounter) of $(MAX_QUESTION)`
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
-    const questionIndex = Math.floor(Math.random() * availableQuestion.length)
-    currentQuestion = availableQuestion[questionIndex]
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
     question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
@@ -83,9 +83,9 @@ getNewQuestion = () =>{
         choice.innerText = currentQuestion['choice' + number]
     })
 
-    availableQuestion.splice(questionIndex, 1)
+    availableQuestions.splice(questionIndex, 1)
 
-    acceptingAnswers = true
+    acceptingAnswers  = true
 }
 
 choices.forEach(choice => {
@@ -99,7 +99,7 @@ choices.forEach(choice => {
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if(classToApply === 'correct') {
-            incrementScore(SCORE_POINT) 
+            incrementScore(SCORE_POINTS) 
         }
 
         selectedChoice.parentElement.classList.add(classToApply)
@@ -107,6 +107,14 @@ choices.forEach(choice => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
+
         }, 1000)
     })
 })
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
